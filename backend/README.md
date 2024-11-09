b415476c2f9ba575f7720bcfb720b52d79d2cf77
Vitisadi <82157788+Vitisadi@users.noreply.github.com>
2024-11-09 12:55:23 -0500

    Created backend

---
+++ backend/README.md
@@ -0,0 +1,4 @@
+http://localhost:8080/sampleGet
+
+http://localhost:8080/sampleGet
+Post request with Value in body
---
+++ backend/routes/sampleGet.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const router = express.Router();
+
+const __filename = fileURLToPath(import.meta.url);
+const parsed = path.parse(__filename);
+
+router.get(`/${parsed.name}`, async (req, res) => {  
+   res.json({ 
+      "hello": "hi",
+      "connected": true 
+   });
+});
+
+export default router;
