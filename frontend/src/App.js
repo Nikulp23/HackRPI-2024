be53be746aff8cf6c5dc114ca7f188db51400979
mrathgeber <mjrathgeber@gmail.com>
2024-11-09 22:45:14 -0500

    robert

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 import FileUploadToggle from './components/buttons/FileUploadToggle'
 import IdentifiedItem from './components/content/IdentifiedItem'
+import leafImg from './images/leaf.png';
 
 function App() {
   // State variables to store the response messages
@@ -49,30 +50,20 @@ function App() {
                   {facts : "Trash bags are made from polyethylene terephthalate (PET) or high-density polyethylene (HDPE), both of which are highly recyclable. Recycling plastic can help reduce pollution and conserve natural resources, as producing new plastic products from recycled materials uses less energy and water compared to making them from raw materials.",
                   infoOnUse : "Trash bags can be recycled at most recycling centers. To properly recycle, rinse and dry the bottle to remove any liquid or residue. Check for a recycling symbol and number on the bottle to ensure it is accepted by your local recycling program.",
                   name : "Trash Bag",
-                  use : "Garbage"}];
+                  use : "Reuse"},
+                
+                  {facts : "Trash bags are made from polyethylene terephthalate (PET) or high-density polyethylene (HDPE), both of which are highly recyclable. Recycling plastic can help reduce pollution and conserve natural resources, as producing new plastic products from recycled materials uses less energy and water compared to making them from raw materials.",
+                    infoOnUse : "Trash bags can be recycled at most recycling centers. To properly recycle, rinse and dry the bottle to remove any liquid or residue. Check for a recycling symbol and number on the bottle to ensure it is accepted by your local recycling program.",
+                    name : "Trash Bag",
+                    use : "Salvage"},
+                  
+                  {facts : "Trash bags are made from polyethylene terephthalate (PET) or high-density polyethylene (HDPE), both of which are highly recyclable. Recycling plastic can help reduce pollution and conserve natural resources, as producing new plastic products from recycled materials uses less energy and water compared to making them from raw materials.",
+                    infoOnUse : "Trash bags can be recycled at most recycling centers. To properly recycle, rinse and dry the bottle to remove any liquid or residue. Check for a recycling symbol and number on the bottle to ensure it is accepted by your local recycling program.",
+                    name : "Trash Bag",
+                    use : "Useless"}];
 
     setData(data);
   }, []);
-
-
-  const fileInputRef = useRef(null);
-  const [imageUrl, setImageUrl] = useState(null); // State to store the uploaded image URL
-
-  const handleUploadBoxClick = () => {
-    fileInputRef.current.click();
-  };
-
-  const handleDiscardImage = () => {
-    setImageUrl(null);
-  }
-
-  const handleFileChange = (event) => {
-    const file = event.target.files[0];
-    if (file) {
-      console.log('Selected file:', file.name);
-      uploadFile(file);
-    }
-  };
 
   const getCroppedData = async (blob, coordinates) => {
     const formData = new FormData();
@@ -98,7 +89,6 @@ function App() {
     }
   };
   
-
   const uploadFile = async (file) => {
     const formData = new FormData();
       formData.append('image', file);
@@ -125,6 +115,52 @@ function App() {
       }
   }
 
+  const fileInputRef = useRef(null);
+  const [imageUrl, setImageUrl] = useState(null);
+  const [videoUrl, setVideoUrl] = useState(null);
+
+  // ---------- file upload state helper functions ---------- //
+  const [fileUploadState, setFileUploadState] = useState('imageUpload');
+
+  const handleUploadBoxClick = () => {
+    fileInputRef.current.click();
+  };
+
+  const handleFileUploadState = (data) => {
+    setFileUploadState(data);
+  };
+
+  // ---------- image helper functions ---------- //
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      console.log('Selected file:', file.name);
+      uploadFile(file);
+    }
+  };
+
+  const handleDiscardImage = () => {
+    setImageUrl(null);
+  }
+
+  // ---------- video helper functions ---------- //
+  const handleVideoChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+        const videoUrl = URL.createObjectURL(file);
+        setVideoUrl(videoUrl);
+    }
+  };
+
+  const handleDiscardVideo = () => {
+    setVideoUrl(null);
+    if (fileInputRef.current) {
+        fileInputRef.current.value = null;
+    }
+  };
+
+  // ---------- ---------- //
+
   return (
     <div className="App">
       <header className="App-header">
@@ -133,57 +169,102 @@ function App() {
           <p><strong>GET Response:</strong> {getMessage}</p>
           <p><strong>POST Response:</strong> {postMessage}</p> */}
 
-          <h2>Eco-Friendly Recycling Buddy</h2>
+          <h2 className="title">Eco-Friendly Recycling Buddy</h2>
         </div>
       </header>
 
       <div className="content">
         <div className="uploadButtons">
-            <FileUploadToggle />
+            <FileUploadToggle onData={handleFileUploadState} />
         </div>
 
         <div className="card">
           <div className="cardContent">
 
-            <h2 className="cardHeader">Upload an Image</h2>
-            <p className="cardText">Let's see what eco-treasures you've found!</p>
-            <div className="uploadBoxContainer">
-              <div className="uploadBox" onClick={handleUploadBoxClick}>
-                {!imageUrl && <h4 className="clickToUpload">Click to upload</h4> }
-                <input
-                  type="file"
-                  ref={fileInputRef}
-                  style={{ display: 'none' }}
-                  onChange={handleFileChange}
-                />
-                {imageUrl &&
-                  <>
-                    <div className="imageDisplayContainer">
-                      <img src={imageUrl} className="imageDisplay" alt="Uploaded" style={{ maxWidth: '95%', maxHeight: '75%' }} />
-                      <div className="reuploadContainer">
-                        <h4 className="clickToUpload">Click to re-upload</h4>
+            {fileUploadState == 'imageUpload' &&
+            <>
+              <h2 className="cardHeader">Upload an Image</h2>
+              <p className="cardText">Let's see what eco-treasures you've found!</p>
+              <div className="uploadBoxContainer">
+                <div className="uploadBox" onClick={handleUploadBoxClick}>
+                  {!imageUrl && <h4 className="clickToUpload">Click to upload</h4> }
+                  <input
+                    type="file"
+                    accept="image/*"
+                    ref={fileInputRef}
+                    style={{ display: 'none' }}
+                    onChange={handleFileChange}
+                  />
+                  {imageUrl &&
+                    <>
+                      <div className="imageDisplayContainer">
+                        <img src={imageUrl} className="imageDisplay" alt="Uploaded" style={{ maxWidth: '95%', maxHeight: '75%' }} />
+                        <div className="reuploadContainer">
+                          <h4 className="clickToUpload">Click to re-upload</h4>
+                        </div>
                       </div>
-                    </div>
-                  </>
+                    </>
+                  }
+                </div>
+                {imageUrl &&
+                <>
+                <div className="reuploadContainer">
+                  <h4 className="clickToUpload">OR</h4>
+                </div>
+                <button className="discardImage" onClick={handleDiscardImage}>
+                  <h4 className="clickToUpload">Discard image</h4>
+                </button>
+                </>
                 }
               </div>
-              {imageUrl &&
-              <>
-              <div className="reuploadContainer">
-                <h4 className="clickToUpload">OR</h4>
+            </>}
+
+            {fileUploadState == 'videoUpload' &&
+            <>
+              <h2 className="cardHeader">Upload a Video</h2>
+              <p className="cardText">Let's see what eco-treasures you've found!</p>
+              <div className="uploadBoxContainer">
+                <div className="uploadBox" onClick={handleUploadBoxClick}>
+                  {!videoUrl && <h4 className="clickToUpload">Click to upload</h4> }
+                  <input
+                    type="file"
+                    accept="video/*"
+                    ref={fileInputRef}
+                    style={{ display: 'none' }}
+                    onChange={handleVideoChange}
+                  />
+                  {videoUrl &&
+                    <>
+                      <div className="imageDisplayContainer">
+                        <video controls src={videoUrl} className="imageDisplay" style={{ maxWidth: '95%', maxHeight: '75%' }} />
+                        <div className="reuploadContainer">
+                          <h4 className="clickToUpload">Click to re-upload</h4>
+                        </div>
+                      </div>
+                    </>
+                  }
+                </div>
+                {videoUrl &&
+                <>
+                <div className="reuploadContainer">
+                  <h4 className="clickToUpload">OR</h4>
+                </div>
+                <button className="discardImage" onClick={handleDiscardVideo}>
+                  <h4 className="clickToUpload">Discard video</h4>
+                </button>
+                </>
+                }
               </div>
-              <button className="discardImage" onClick={handleDiscardImage}>
-                <h4 className="clickToUpload">Discard image</h4>
-              </button>
-              </>
-              }
-            </div>
+            </>}
           </div>
         </div>
 
-        <div className="card" style={{maxHeight: "60vh", overflowY: "auto", overflowX: "hidden"}}>
+        <div className="card" style={{maxHeight: "80vh", overflowY: "auto", overflowX: "hidden"}}>
           <div className="cardContent">
-            <h2 className="cardHeader">Eco-Discoveries</h2>
+            <div className="ecoDiscvTitle">
+              <img src={leafImg} alt="leaf" />
+              <h2 className="cardHeader">Eco-Discoveries</h2>
+            </div>
             <p className="cardSubHeader">Let's see what we can save from the landfill!</p>
 
             {data[0] && <IdentifiedItem facts={data[0].facts}
@@ -196,6 +277,18 @@ function App() {
                             infoOnUse={data[1].infoOnUse}
                             name={data[1].name}
                             use={data[1].use}
+                            />}
+
+            {data[2] && <IdentifiedItem facts={data[2].facts}
+                            infoOnUse={data[2].infoOnUse}
+                            name={data[2].name}
+                            use={data[2].use}
+                            />}
+
+            {data[3] && <IdentifiedItem facts={data[3].facts}
+                            infoOnUse={data[3].infoOnUse}
+                            name={data[3].name}
+                            use={data[3].use}
                             />}
 
           </div>
